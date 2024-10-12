ebbe45a361310642068c4939509f6d4012bd4c53
sofolitltd <sofolitltd@gmail.com>
2024-10-12 23:09:10 +0600

    first commit

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //logo, notification
+      appBar: AppBar(
+        // logo
+
+        // notifications
+        actions: [],
+      ),
+
+      //
+      body: const Column(
+        children: [
+          // search blood
+
+          // post , bank , donner
+
+          // community
+
+          // feed
+        ],
+      ),
+    );
+  }
+}
